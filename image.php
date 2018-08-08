6603912e41c4b5918c82c66a60a5d3b53130f3b3
Folkmann <theme1256@gmail.com>
2018-08-08 14:25:47 +0200

    Opdatering til hent af billede, så der er test, hvis tom

--- image.php
+++ image.php
@@ -1,21 +1,25 @@
 <?php
-include("include/authcheck.php");
-include("config/config.php");
-header('Content-Encoding: gzip');
-if (isset($_GET["cameraId"])) $cameraId = $_GET["cameraId"];
-else $cameraId = "";
-if (isset($_GET["width"]))$width = $_GET["width"];
-else $width = "720";
-$arrContextOptions=array(
-    "ssl"=>array(
-        "verify_peer"=>false,
-        "verify_peer_name"=>false,
-    ),
-);
-if (isset($_GET["cameraId"])) {
-$url = 'https://'.$domain.':'.$port.'/api/2.0/snapshot/camera/'.$cameraId.'?force=true&width='.$width.'&apiKey='.$apiKey;
-$im = file_get_contents($url, false, stream_context_create($arrContextOptions));
-echo gzencode(base64_encode($im),$compressionLevel);
-}
-else echo "";
+	include("include/authcheck.php");
+	include("config/config.php");
+	header('Content-Encoding: gzip');
+	if (isset($_GET["cameraId"])) $cameraId = $_GET["cameraId"];
+	else $cameraId = "";
+	if (isset($_GET["width"]))$width = $_GET["width"];
+	else $width = "720";
+	$arrContextOptions = array(
+		"ssl"=>array(
+			"verify_peer"=>false,
+			"verify_peer_name"=>false,
+		),
+	);
+	if(isset($_GET["cameraId"])){
+		$url = 'https://'.$domain.':'.$port.'/api/2.0/snapshot/camera/'.$cameraId.'?force=true&width='.$width.'&apiKey='.$apiKey;
+		$im = file_get_contents($url, false, stream_context_create($arrContextOptions));
+		if(empty($im)){
+			$im = file_get_contents("img/dead.jpg");
+		}
+			echo gzencode(base64_encode($im), $compressionLevel);
+	} else{
+		echo "";
+	}
 ?>
