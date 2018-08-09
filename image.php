31595224a6ec78f13ce8fd148722d74eefde26ea
Folkmann <theme1256@gmail.com>
2018-08-09 14:37:38 +0200

    Lille opdatering til at håndtere cachede billeder

--- image.php
+++ image.php
@@ -13,12 +13,28 @@
 		),
 	);
 	if(isset($_GET["cameraId"])){
-		$url = 'https://'.$domain.':'.$port.'/api/2.0/snapshot/camera/'.$cameraId.'?force=true&width='.$width.'&apiKey='.$apiKey;
-		$im = file_get_contents($url, false, stream_context_create($arrContextOptions));
-		if(empty($im)){
+		try{
+			$url = 'https://'.$domain.':'.$port.'/api/2.0/snapshot/camera/'.$cameraId.'?force=true&width='.$width.'&apiKey='.$apiKey;
+			$im = @file_get_contents($url, false, stream_context_create($arrContextOptions));
+			if(empty($im) || var_export($im, true) == false || json_decode($im)["rc"] == "error"){
+				$im = file_get_contents("img/dead.jpg");
+			} else{
+				$cached = "cached/".$cameraId.".jpg";
+				if(is_file($cached)){
+					// Sammenlign det hentede med det gemte
+					$prev = file_get_contents($cached);
+					if($prev == $im)
+						$im = file_get_contents("img/dead.jpg");
+				} else{
+					// Opret filen
+					file_put_contents($cached, $im);
+				}
+			}
+		} catch(Exception $ex){
 			$im = file_get_contents("img/dead.jpg");
 		}
-			echo gzencode(base64_encode($im), $compressionLevel);
+		echo gzencode(base64_encode($im), $compressionLevel);
+		// curl_close($ch);
 	} else{
 		echo "";
 	}
