34a28e2cd8b44e0265fa4d8eb2d88fc7b56c8af2
Folkmann <theme1256@gmail.com>
2019-01-18 14:05:54 +0100

    Flere rettelser til setup

--- config/setup.sh
+++ config/setup.sh
@@ -2,12 +2,14 @@ sudo mkdir /home/pi/.config/lxsession
 sudo mkdir /home/pi/.config/lxsession/LXDE-pi
 sudo mkdir /home/pi/.config/autostart
 
+sudo rm /home/pi/.config/lxsession/LXDE-pi/autostart
+sudo touch /home/pi/.config/lxsession/LXDE-pi/autostart
 sudo echo "@xset s 0 0" >> /home/pi/.config/lxsession/LXDE-pi/autostart
 sudo echo "@xset s noblank" >> /home/pi/.config/lxsession/LXDE-pi/autostart
 sudo echo "@xset s noexpose" >> /home/pi/.config/lxsession/LXDE-pi/autostart
 sudo echo "@xset dpms 0 0 0" >> /home/pi/.config/lxsession/LXDE-pi/autostart
 
-sudo mkdir /home/pi/.config/autostart
+sudo rm /home/pi/.config/autostart/autoChromium.desktop
 sudo touch /home/pi/.config/autostart/autoChromium.desktop
 
 sudo echo "[Desktop Entry]" >> /home/pi/.config/autostart/autoChromium.desktop
@@ -23,7 +25,7 @@ sudo chown pi:pi /home/pi/.config -R
 
 sudo apt install apache2 -y
 sudo a2enmod rewrite
-sudo sed -i 's/<Directory /var/www/>\n        Options Indexes FollowSymLinks\n        AllowOverride None/<Directory /var/www/>\n        Options Indexes FollowSymLinks\n        AllowOverride All/g' /etc/apache2/apache.conf
+sudo echo 's/<Directory \/var\/www\/>\n	Options Indexes FollowSymLinks\n	AllowOverride None/<Directory /var/www/>\n	Options Indexes FollowSymLinks\n	AllowOverride All/g' >> /etc/apache2/apache2.conf
 sudo service apache2 restart
 
 sudo apt install php libapache2-mod-php -y
